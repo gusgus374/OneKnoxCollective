343a641a2a07094bbdbe679709e1968dacaed5da
gusgus374 <gusalvarez374@gmail.com>
2024-02-28 11:23:50 -0500

    commented out some stuff

--- home.py
+++ home.py
@@ -22,36 +22,36 @@ path = os.path.join(str(pathlib.Path().resolve()), './okycgeoGM_color.csv')
 with open(path) as f:
     data = pd.read_csv(f)
 chart_data = pd.DataFrame(data)
-"""
-st.pydeck_chart(pdk.Deck(
-    map_style=None,
-    initial_view_state=pdk.ViewState(
-        latitude=35.964668,
-        longitude=-83.926453,
-        zoom=11,
-        pitch=50,
-    ),
-    layers=[
-        pdk.Layer(
-           'HexagonLayer',
-           data=chart_data,
-           get_position='[longitude, latitude]',
-           radius=200,
-           elevation_scale=4,
-           elevation_range=[0, 1000],
-           pickable=True,
-           extruded=True,
-        ),
-        pdk.Layer(
-            'ScatterplotLayer',
-            data=chart_data,
-            get_position='[longitude, latitude]',
-            get_color='[200, 30, 0, 160]',
-            get_radius=200,
-        ),
-    ],
-))
-"""
+
+#st.pydeck_chart(pdk.Deck(
+#    map_style=None,
+#    initial_view_state=pdk.ViewState(
+#        latitude=35.964668,
+#        longitude=-83.926453,
+#        zoom=11,
+#        pitch=50,
+#    ),
+#    layers=[
+#        pdk.Layer(
+#           'HexagonLayer',
+#           data=chart_data,
+#           get_position='[longitude, latitude]',
+#           radius=200,
+#           elevation_scale=4,
+#           elevation_range=[0, 1000],
+#           pickable=True,
+#           extruded=True,
+#        ),
+#        pdk.Layer(
+#            'ScatterplotLayer',
+#            data=chart_data,
+#            get_position='[longitude, latitude]',
+#            get_color='[200, 30, 0, 160]',
+#            get_radius=200,
+#        ),
+#    ],
+#))
+
 
 '''
 # One Knox Youth Club Player Database
